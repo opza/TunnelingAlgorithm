4b8a305c48a84f3cff7ae421845f8652d67d4514
opza <mingu991114@gmail.com>
2023-03-29 08:08:52 +0900

    WPFPrinter Json 종속성 추가

---
+++ Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Test
+{
+    public record MyPosition
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public MyPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
