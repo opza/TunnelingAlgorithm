c20de3134f58dc46280d2e56f1575784a73442c7
opza <mingu991114@gmail.com>
2023-09-07 17:54:46 +0900

    Readme 정리

--- ConsolePrinter/Program.cs
+++ ConsolePrinter/Program.cs
@@ -1,4 +1,40 @@
-﻿using TunnelingAlgorithm.Configurations;
+﻿using TunnelingAlgorithm;
+using TunnelingAlgorithm.Configurations;
 
-var path = "D:\\Code\\TunnelingAlgorithm\\TunnelingAlgorithm\\Param.json";
-var config = Config.Read(path);
+var width = 50;
+var height = 50;
+var path = "E:\\Code\\Other\\TunnelingAlgorithm\\Param.json";
+
+var worldGenerator = new WorldGenerator(path);
+(var tiles, var rooms) = worldGenerator.Generate(width, height);
+
+using var ws = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+for (int x = 0; x < width; x++)
+{
+	for (int y = 0; y < height; y++)
+	{
+		switch (tiles[x,y])
+		{
+			case TileType.Rock:
+				ws.Write("@");
+				break;
+			case TileType.Corridor:
+				ws.Write(".");
+				break;
+			case TileType.Room:
+				ws.Write(".");
+				break;
+			case TileType.Wall:
+				ws.Write("#");
+				break;
+			case TileType.Door:
+				ws.Write("&");
+				break;
+			default:
+				break;
+		}
+	}
+
+	ws.WriteLine();
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+## 터널링 알고리즘을 사용한 절차적 맵 생성
+![vdeio](https://www.reddit.com/link/11klbmf/video/b7cjegy328ma1/player)  
+
+### 요구버전
+#### ConsolePrinter
+\>= .Net 6.0  
+\>= C# 9.0  
+  
+#### TunnelingAlgorithm
+\>= .Net Standard 2.1  
+
+#### WPFPrinter
+\>= .Net 6.0  
+\>= C# 9.0  
+
+개인 개발용으로 사용중
+
+---
+### Reference
+https://www.reddit.com/link/11klbmf/video/b7cjegy328ma1/player
+https://dungeonmaker.sourceforge.net/DM2_Manual/index.html
